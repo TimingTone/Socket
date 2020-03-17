0484a49b31ca413b7e22f2f353e9580db136c726
TimingTone <1725491705@qq.com>
2020-03-17 13:10:58 +0800

    #

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 Platform: win10
 
 Software:  Visual Studio 2019
+
+# Introduction
+
+EasyTcpClient/EasyTcpServer: 介绍基本Tcp网络通信, 使用SocketAPI6初步建立Tcp服务端/客户端
+
+TcpClientPlus/TcpServerPlus: 建立能持续处理请求的CS网络程序
+
+TcpClientStruct/TcpServerStruct: 发送结构化的网络消息数据
+
+TcpClientStructPlus/TcpServerStructPlus: 网络报文的数据格式定义及使用
---
+++ TcpClientStructPlus/Client_4.cpp
@@ -0,0 +1,130 @@
+#define WIN32_LEAN_AND_MEAN //引入宏去避免早期的会引起冲突的一些库，减少其他依赖库的引用
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <WinSock2.h>
+#include <windows.h>
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+
+
+//用Socket API建立简易的TCP客户端
+
+enum CMD {
+	CMD_LOGIN,
+	CMD_LOGINOUT,
+	CMD_ERROR
+
+};
+struct DataHeader {
+	short dataLength;
+	short cmd;
+};
+//DataPackage
+struct Login {
+	char userName[32];
+	char passWord[32];
+};
+
+struct LoginResult {
+	int result;
+
+};
+
+struct LoginOut {
+	char userName[32];
+};
+
+struct LoginOutResult {
+	int result;
+};
+
+int main() {
+
+	//启动windows socket 2.x环境
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	//The WSADATA structure is used to save the Windows Sockets
+	// initialization information returned by the function WSAStartup
+	WSADATA wsadata;
+
+	//调用winSocket的启动函数WSAStartup
+	//wVersionRequested  ：WORD的版本号
+	//&wsadata ：WSADATA的数据结构(struct)
+	WSAStartup(wVersionRequested, &wsadata);
+	//用Socket API建立简易的TCP客户端
+	// 1. 建立一个socket
+	SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);  //_sock是服务端socket
+	if (INVALID_SOCKET == _sock) {
+		printf("Failed to establish socket client...\n");
+	}
+	else {
+		printf("Succeed to establish the client...\n");
+	}
+	// 2. 连接服务器 connect
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(4567);
+	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
+	if (SOCKET_ERROR == ret) {
+		printf("Failed to establish socket client...\n");
+	}
+	else {
+		printf("Succeed to establish the client...\n");
+	}
+
+	// 3, Input request command
+	while (true) {
+		char cmdBuf[128] = {};
+		scanf("%s", cmdBuf);
+		// 4, Deal with request command
+		if (0 == strcmp(cmdBuf, "exit")) {
+			printf("Received quit command!");
+			break;
+		}
+		else if (0 == strcmp(cmdBuf, "login")) {
+			Login login = { "lyd", "lydmm" }; //login.userName = "lyd",     login.passWord = "lydmm"
+			DataHeader dh = { sizeof(login),  CMD_LOGIN };
+			// 5, send request command to server.
+			send(_sock, (const char*)&dh, sizeof(dh), 0);
+			send(_sock, (const char*)&login, sizeof(login), 0);
+			//  receive the data returned form server
+			DataHeader retHeader = {};
+			LoginResult loginRet = {};
+			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			recv(_sock, (char*)&loginRet, sizeof(loginRet), 0);
+			printf("LoginResult:  %d \n", loginRet.result);
+		}
+		else if (0 == strcmp(cmdBuf, "loginOut")) {
+			LoginOut loginOut = {"lyd"};
+			DataHeader dh = { sizeof(loginOut), CMD_LOGINOUT };
+			// 5, send request command to server.
+			send(_sock, (const char*)&dh, sizeof(dh), 0);
+			send(_sock, (const char*)&loginOut, sizeof(loginOut), 0);
+			//  receive the data returned form server
+			DataHeader retHeader = {};
+			LoginResult loginOutRet= {};
+			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			recv(_sock, (char*)&loginOutRet, sizeof(loginOutRet), 0);
+			printf("LoginResult:  %d \n", loginOutRet.result);
+		}
+		else {
+			printf("Riceive unsupport command, please enter again\n");
+		}
+		// 6. 接收服务器信息recv
+		//char recvBuf[128] = {};
+		//int nlen = recv(_sock, recvBuf, 128, 0);
+		//if (nlen > 0) {
+		//	//recvBuf is a binary data, must be converted
+		//	DataPackage* dp = (DataPackage*)recvBuf;
+		//	printf("Age: %d    \nName = %s\n", dp->age, dp->name);
+		//}
+	}
+
+
+	// 7. 关闭socket  closesocket
+	closesocket(_sock);
+	WSACleanup();
+	printf("quit!, mission completed.");
+	getchar();
+	return 0;
+}
